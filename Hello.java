3d1c9f9fcceb3d9c19c200b489b5faf72c98b449
Raghav973171 <tripathiraghav688@gmail.com>
2025-08-10 19:44:10 +0530

    Hello.java file added

---
+++ Hello.java
@@ -0,0 +1,22 @@
+public class Hello {
+
+    int a = 20; 
+    int b=30;
+
+
+
+int add(){
+
+
+return this.a+this.b;
+
+}
+
+    public static void main(String args[]) {
+
+        Hello obj = new Hello();
+        
+
+        System.out.println(obj.add()); 
+}
+}
